ff02d24d5a9c12659270a0d03114800078b7f193
Per Ivar Nerseth <perivar@nerseth.com>
2015-05-30 14:59:34 +0200

    Fixed the command line execution

--- Program.cs
+++ Program.cs
@@ -4,13 +4,21 @@ namespace ExtractMassiveWavetables
 {
 	class Program
 	{
+		const string _version = "0.1";
+		
 		public static void Main(string[] args)
 		{
-			MassiveDatFile.Extract(@"C:\Users\perivar.nerseth\OneDrive\Audio\Audio Software\Native Instruments\Massive\tables.dat", @"c:\temp");
-
-			Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
+			if (args.Length < 2) {
+				System.Console.Out.WriteLine("Exctract NI Massive Wavetables (and other content from tables.dat).");
+				System.Console.Out.WriteLine("Version {0}", _version);
+				System.Console.Out.WriteLine();
+				System.Console.Out.WriteLine("Usage ExtractMassiveWavetables.exe <path-to-tables.dat> <path-to-output-directory>");
+				return;
+			}
 			
+			if (!MassiveDatFile.Extract(args[0], args[1])) {
+				System.Console.WriteLine("Massive's tables.dat not found!");
+			}
 		}
 	}
 }
