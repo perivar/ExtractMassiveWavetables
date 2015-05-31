8ddf390a926549e86aca838b0e3a1bae5fa52f23
Per Ivar Nerseth <perivar@nerseth.com>
2015-05-31 18:47:22 +0200

    Added missing file

---
+++ RUN-massive.py.bat
@@ -0,0 +1,4 @@
+@echo off
+cd "G:\\Development Projects\\Portable Python 2.7.6.1"
+
+Python-Portable.exe "G:\Development Projects\Own Projects\ExtractMassiveWavetables\massive.py" "G:\\SkyDrive\\Audio\\Audio Software\\Native Instruments\\Massive\\tables.dat" "C:\Temp\Original"
